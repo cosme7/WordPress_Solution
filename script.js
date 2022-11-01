099a39b7adb42f714de23b2fe6df33de4ab6c585
cosme7 <cosmewr@gmail.com>
2022-11-01 07:33:46 -0300

    First Commit + Header 100%

---
+++ script.js
@@ -0,0 +1,11 @@
+const headerContainer = document.querySelector('.h--container');
+const navToggle = document.querySelector('.mobile-nav-toogle');
+const primaryNav = document.querySelector('.primary-nav');
+
+navToggle.addEventListener('click', () => {
+    primaryNav.hasAttribute("data-visible") 
+    ? navToggle.setAttribute("aria-expanded", false)
+    : navToggle.setAttribute("aria-expanded", true)
+    primaryNav.toggleAttribute("data-visible");
+    headerContainer.toggleAttribute("data-overlay");
+})
